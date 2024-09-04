c06a81b04b3cbd6cf5c6f16537fb60c55688477c
Flynn070 <Phoenix700@hotmail.com>
2024-09-04 05:08:50 +0100

    commit from vs test

--- Forecast.cs
+++ Forecast.cs
@@ -32,7 +32,7 @@ namespace FormeCastWeatherApp
         //mean sea level pressure
         float mslp; 
 
-        float uvIndex;
+        int uvIndex;
 
         // -1 to 30, different meanings for all
         int significantWeatherCode;
@@ -47,10 +47,38 @@ namespace FormeCastWeatherApp
         string[] sigWeatherCodes = { "Clear night", "Sunny day", "Partly cloudy (night)", "Partly cloudy (day)", "Not Used", "Mist", "Fog", "Cloudy", "Overcast", "Light rain shower (night)", "Light rain shower (day)", "Drizzle", "Light rain", "Heavy rain shower (night)", "Heavy rain shower (day)", "Heavy rain", "Sleet shower (night)", "Sleet shower (day)", "Sleet", "Hail shower (night)", "Hail shower (day)", "Hail", "Light snow shower (night)", "Light snow shower (day)", "Light snow", "Heavy snow shower (night)", "Heavy snow shower (day)", "Heavy snow", "Thunder shower (night)", "Thunder shower (day)", "Thunder", "Trace rain"};
         string significantWeather = "";
 
+        string uvIndexAdvice = "";
+
+
         public void SetSignificantWeatherCode(int NewCode)
         {
             this.significantWeatherCode = NewCode;
             this.significantWeather = sigWeatherCodes[NewCode];     //gets the correct weather for the code
+        }
+
+        public void SetUVIndex(int NewIndex)
+        {
+            this.uvIndex = NewIndex;
+            if (NewIndex <= 3)
+            {
+                this.uvIndexAdvice = "Low exposure. No protection required. You can safely stay outside";
+            }
+            else if (NewIndex <= 5)
+            {
+                this.uvIndexAdvice = "Moderate exposure. Seek shade during midday hours, cover up and wear sunscreen";
+            }
+            else if (NewIndex <= 7)
+            {
+                this.uvIndexAdvice = "High exposure. Seek shade during midday hours, cover up and wear sunscreen";
+            }
+            else if (NewIndex <= 10)
+            {
+                this.uvIndexAdvice = "Very high. Avoid being outside during midday hours. Shirt, sunscreen and hat are essential";
+            }
+            else
+            {
+                this.uvIndexAdvice = "Extreme. Avoid being outside during midday hours. Shirt, sunscreen and hat essential."
+            }
         }
 
         //TODO write setters and getters
